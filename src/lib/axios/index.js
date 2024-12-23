9f10e27f6590a5192579d6e9ea83ebbf90e9f4b4
Төр-Эрдэнэ Сурмаажав <24LP1247@pinecones-iMac.local>
2024-12-23 16:10:36 +0800

    deployment

---
+++ src/lib/axios/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+  // baseURL: "//localhost:5000", // Use http for localhost unless you have HTTPS set up
+  baseURL:"https://expense-trackertu.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
